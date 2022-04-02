4675e9dee25a225b6fafc3baccde18046d323302
msuhailf <msuhailf@deloitte.com>
2022-04-02 23:08:45 +0530

    No change

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import logging
 import ConsumerDemo
 import ProducerDemo
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ProducerDemo.sample_producer()
-    print('Program completed successfully')
+    print("Program completed successfully")
